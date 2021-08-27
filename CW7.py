8f62aac6f98a50156eee4db3ff678d1ce9cf69ed
Adil Mohammed K <adilmohammed2000@outlook.com>
2021-08-27 11:43:37 +0530

    Keywords, pos, variable keywords and positional

--- CW7.py
+++ CW7.py
@@ -1,3 +1,51 @@
+def pos_fun(i,f,st):
+    su1 = i + f
+    st1 = st.upper()
+    return su1, st1
+
+out1 , out2 = pos_fun(5,6.53,'Adil')
+print(out1,out2)
+
+out3 = pos_fun(10,12.5,'Cool') # Return as tuple
+print(out3)
+
+# Keyword argument
+
+out_lst1 = pos_fun(i=10,f=12.53,st = 'Adil')
+print(out_lst1)
+out_lst2 = pos_fun(i=10,st = 'Adil',f=13.56)
+print(out_lst2)
+
+out1,out2 = pos_fun(10,f=12.5,st = 'Adil')
+print(out1,out2)
+# out1,out2 = pos_fun(i=12,10.5,st = 'Adil') # Does not work
+# print(out1,out2)
+
+# Variable Positional 
+def var_args(*args):
+    for var in args:
+        print(var,type(var))
+
+var_args('Adil',1,5)
+var_args(12.5,5,'Lidar')
+
+def kwvar_args(**kwargs): # Takes it as dict
+    for k,v in kwargs.items():
+        print(k,v)
+
+kwvar_args(i=10)
+kwvar_args(i=10,j=12.5)
+kwvar_args(st1='Adil',i=12,j=34.5)
+
+def pvkv(i,j,*args,k,**kwargs):
+    print(i,j)
+    for var in args:
+        print(var)
+    print(k)
+    for k,v in kwargs.items():
+        print(k,v)
+
+pvkv(10,52,5,'Adil','Mohammed',k=567,l=43,m=56.67)
 # Recursive Functions
 
 def PrintNum(N):
